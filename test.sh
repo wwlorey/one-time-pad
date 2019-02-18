4a534c8959a21552fd804e08cf55a754f49a2476
William Lorey <wwlorey@gmail.com>
2019-02-18 16:05:27 -0600

    Update testing script, truncate test input file to 2048 bits

--- test.sh
+++ test.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+KEY_ID='5'
+
 ./bin_otp_key_gen.py
-./bin_otp.py keyfile.sec 50 input.txt encrypted.txt
-./bin_otp.py keyfile.sec 50 encrypted.txt decrypted.txt
+./bin_otp.py keyfile.sec $KEY_ID input.txt encrypted.txt
+./bin_otp.py keyfile.sec $KEY_ID encrypted.txt decrypted.txt
+diff input.txt decrypted.txt
